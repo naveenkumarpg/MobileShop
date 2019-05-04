b7d8de82277579beb45214e856ad6ed782289942
Naveen Setty <nsetty@sapient.com>
2019-05-04 23:24:32 +0530

    Adding Build files.

---
+++ build/precache-manifest.8a4ffd40978ff25f3703337c76dd10f6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "bab5332b1c431da0208c",
+    "url": "/static/css/main.2abf819b.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "df795f367e3a03da8955",
+    "url": "/static/js/2.df795f36.chunk.js"
+  },
+  {
+    "revision": "bab5332b1c431da0208c",
+    "url": "/static/js/main.bab5332b.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "df795f367e3a03da8955",
+    "url": "/static/css/2.82f1fc88.chunk.css"
+  },
+  {
+    "revision": "d82a0ad86451a0e92f9ea11aead0348b",
+    "url": "/index.html"
+  }
+];
