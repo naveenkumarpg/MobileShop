c6c4916f23baf586324de594b98cf922f1213ff0
Naveen Setty <nsetty@sapient.com>
2019-05-14 15:05:20 +0100

    UI | adding new labels for 404 page.

--- src/Labels/labels.js
+++ src/Labels/labels.js
@@ -5,5 +5,7 @@ export default {
     "cart": "Cart",
     "defValue":0,
     "PGF":"Page Not Found",
-    "disclaimer":"&copy; 2019 +Mobile shop"
+    "disclaimer":"&copy; 2019 +Mobile shop",
+    "notfounddesc" : "Sorry, we couldn't find the page you were looking for. we suggest that you return to main section.",
+    "mainpage" : "Go to the Main Page"
   };
