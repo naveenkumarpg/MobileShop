76eafad44d5d345a3849dc3ad19557ae56ab1827
Naveen Setty <nsetty@sapient.com>
2019-05-20 09:15:53 +0100

    UI | Added error page.

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../Error/Error.css';
+
+import '../Cart/Cart.css';
 
 import Labels from '../../Labels/labels';
 
 export default class Default extends Component {
   render() {
     return (
-      <div className="container">
-        {Labels.PGF}
+      <div className="container text-center">
+        <div className="hgap"></div>
+        <h3>{Labels.PGF}</h3>
+        <div className="hgap"></div>
+        <img className='notfoundimg' src="images/404.png" alt="404 artifact" />
+        <div className="hgap"></div>
+        <p>{Labels.notfounddesc}</p>
+        <div className="hgap"></div>
+        <a href="/" className="btn btn-primary" >{Labels.mainpage}</a>
+        <div className="hgap"></div>
       </div>
     )
   }
